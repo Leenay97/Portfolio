457f33ff0f5734916763455110588b7bc8234930
Leenay97 <skynekop@gmail.com>
2021-10-18 01:34:39 +0800

    started

---
+++ js/script.js
@@ -0,0 +1,32 @@
+/*$(function(){
+
+    var str = 'Hey, nice to see you!<br>Welcome to my portfolio website!<br>Want to see more? Press Y/N ',
+        i = 0,
+        isTag,
+        text;
+
+    (function type(){
+        text = str.slice(0, ++i) + '|';
+        if (text === str) return;
+
+        document.getElementById('hello-screen').innerHTML = text;
+
+        var char = text.slice(-1);
+        if( char === '<' ) isTag = true;
+        if( char === '>' ) isTag = false;
+    
+        if (isTag) return type();
+        setTimeout(type, 40);
+        
+    }());
+    window.addEventListener('keypress', function(){
+    if (event.key === 'y') {
+        $('.hello-screen').hide();
+        $('main').show();
+    }else if (event.key === 'n') {
+        $('header').append('Bye');
+        return false;
+    };
+    
+    });
+});*/
