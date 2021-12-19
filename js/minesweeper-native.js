25b09d0d9c83ecc3d907098ce78cad9c26574a87
Leenay97 <skynekop@gmail.com>
2021-12-20 02:15:49 +0800

    saper is now in native

--- js/minesweeper-native.js
+++ js/minesweeper-native.js
@@ -3,14 +3,13 @@ document.addEventListener('DOMContentLoaded', () => {
     let gameStopper = 0,
         cellsArr = [],
         stop = 0,
-        timeStop = 0,
-        time = 0,
         bombs = 0,
         bombId = [],
         firstClickIndex = 0,
         firstClick,
         bombStopper = 0,
-        cells = document.querySelectorAll('.game__field-cell');
+        cells = document.querySelectorAll('.game__field-cell'),
+        timer;
     //Null cells
     let nullCells = function () {
         for (let i = 0; i < 9; i++) {
@@ -19,6 +18,9 @@ document.addEventListener('DOMContentLoaded', () => {
                 cellsArr[i][j] = 0;
             };
         };
+        bombId = [];
+        bombs = 0;
+        bombStopper = 0;
     };
     nullCells();
 
@@ -28,11 +30,14 @@ document.addEventListener('DOMContentLoaded', () => {
             if (firstClickIndex == 0) {
                 firstClick = e.target;
                 firstClickIndex = 1;
-                console.log(firstClick.id);
-                e.target.classList.remove('closed-cell');
-            };
-            openCells();
+            
+            let row = Math.floor(e.target.id / 10);
+            let col = e.target.id % 10;
+
+
             gameStart();
+            openCells(row, col);
+            };
         });
     });
 
@@ -88,23 +93,26 @@ document.addEventListener('DOMContentLoaded', () => {
             && row <= 8
             && col >= 0
             && col <= 8;
-    };
+    }
 
-    let openCells = function (row, col) {
+    function openCells(row, col) {
+
         if (!isValid(row, col)) return;
-        let cell = document.getElementById(row + '' + col);
-        
+        var cell = document.getElementById(row + '' + col);
 
-        if (cell.classList.contains('closed-cell')) return;
 
+        if (!cell.classList.contains('closed-cell')) return;
         cell.classList.remove('closed-cell');
-        console.log(cellsArr[row][col])
+
+        if (cells[row][col] >= 100) return;
+
+
         if (cellsArr[row][col] !== 0) return;
 
-        
+
         for (let x = -1; x <= 1; x++) {
             for (let y = -1; y <= 1; y++) {
-                openCells(row + y, col + x);
+                openCells(row + x, col + y);
             }
         }
 
@@ -116,16 +124,61 @@ document.addEventListener('DOMContentLoaded', () => {
             item.addEventListener('click', (e) => {
                 let row = Math.floor(e.target.id / 10);
                 let col = e.target.id % 10;
+                if (openCells(row, col) == false) return;
                 openCells(row, col);
-
             });
         });
     };
+
+    //Timer
+    let startTimer = function () {
+        let timerField = document.querySelector('.game__time');
+        let timerDigit1 = document.querySelector('.time1');
+        let timerDigit2 = document.querySelector('.time2');
+        let timerDigit3 = document.querySelector('.time3');
+        let digit1Back = 17;
+        let digit2Back = 17;
+        let digit3Back = 17;
+        timerDigit1.style.backgroundPosition = '-153px';
+        timerDigit2.style.backgroundPosition = '-153px';
+        timerDigit3.style.backgroundPosition = '-153px';
+        timer = setInterval(function () {
+            digit1Back -= 17;
+            timerDigit3.style.backgroundPosition = digit1Back + 'px';
+            console.log(digit1Back);
+            if (digit1Back == -153) {
+                digit1Back = 17;
+                digit2Back -= 17;
+                timerDigit2.style.backgroundPosition = digit2Back + 'px';
+                if (digit2Back == -153){
+                    digit2Back = 17;
+                    digit3Back -= 17;
+                    timerDigit1.style.backgroundPosition = digit3Back + 'px';
+                }
+            }
+        }, 1000);
+    }
+
+    //Restart
+    function gameRestart() {
+        clearInterval(timer);
+        let restartCells = document.querySelectorAll('.game__field-cell');
+        restartCells.forEach((item) => {
+            item.className = 'game__field-cell closed-cell';
+        });
+        nullCells();
+        gameStart();
+    }
+
+    document.querySelector('.game__face').addEventListener('click', (e)=> {
+        gameRestart();
+    })
 
     //Start game 
     let gameStart = function () {
         arrBombs();
         bombCounter();
+        startTimer();
         cellClick();
     }
 
